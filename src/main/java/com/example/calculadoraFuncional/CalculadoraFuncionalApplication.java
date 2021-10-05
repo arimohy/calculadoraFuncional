3b0b070c18e1afe880270a04600aa9b3ab9cf42e
arimohy <arimohy@gmail.com>
2021-10-05 16:59:38 -0500

    funcion de division

--- src/main/java/com/example/calculadoraFuncional/CalculadoraFuncionalApplication.java
+++ src/main/java/com/example/calculadoraFuncional/CalculadoraFuncionalApplication.java
@@ -1,5 +1,6 @@
 package com.example.calculadoraFuncional;
 
+import java.util.Scanner;
 import java.util.function.IntBinaryOperator;
 import java.util.stream.IntStream;
 
@@ -15,19 +16,22 @@ public class CalculadoraFuncionalApplication {
 		return repeticion;
 	};
 	static IntBinaryOperator div = (a, b) -> {
-		Integer repeticion= IntStream.range(0, Math.abs(b) + 1)
-				.reduce((acumulado, number) ->
-						suma.applyAsInt(Math.abs(a), acumulado)).getAsInt();
-		if((a<0 && b>0)|| (a>0 && b<0))
-			return resta.applyAsInt(0,repeticion);
-		return repeticion;
+		return IntStream.range(0, Math.abs(a) + 1)
+				.reduce((acumulado, number) -> {
+					if(mult.applyAsInt(number,b)<=a)
+						acumulado++;
+					return acumulado;
+				}).getAsInt();
+
 	};
 
 	public static void main(String[] args) {
+		System.out.println("=======CALCULADORA FUNCIONAL========");
+		System.out.println("Ejemplos de operaciones");
 		System.out.println(suma.applyAsInt(-3, 2));
         System.out.println(resta.applyAsInt(-3, 2));
-		System.out.println(mult.applyAsInt(5, 10));
-		System.out.println(div.applyAsInt(5, -10));
+		System.out.println(mult.applyAsInt(5, -7));
+		System.out.println(div.applyAsInt(101, 10));
 	}
 
 }
